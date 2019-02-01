1f628ce0032c0848206f4dac6c6a36f6e03230c6
Bryan G. Lee <bryanlee@tosblock.com>
2019-02-01 09:23:55 +0900

    Fixed some wrong spell.

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
-#
-#  Makefile
-#  lockop-core
-#
-#  Created by 0xbrylee on 2019-02-01.
-#  Copyright (c) 2019 brylabs. All rights reserved.
-#
+//
+//  main.cpp
+//  lockop-core
+//
+//  Created by 0xbrylee on 2019-02-01.
+//  Copyright (c) 2019 brylabs. All rights reserved.
+//
 
 #include <iostream>
 #include "main.h"
