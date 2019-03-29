9e88028afadbb937c6a1bb46df2bd59014cc474c
Bryan G. Lee <bryanlee@tosblock.com>
2019-03-29 19:14:05 +0900

    Base58 encode functions added which is related with address generator func.

--- src/address.cpp
+++ src/address.cpp
@@ -17,35 +17,37 @@ namespace lockop{
 
     std::stringstream& CAddressManager::getAddress(){
         // get address
-        cout << "TEST " << this->mRsultPrivateKey.str() << endl;
-     
+        std::cout << "TEST " << this->mRsultPrivateKey.str() << std::endl;
     }
 
     std::string CAddressManager::generateAddress(){
         CTimeManager time;
         CUtilManager util;
 
+// AA3DC07413 4DF11D31E2 A421BAFF37 01 -> 32 bytes
+// AA3DC074134DF11D31E2A421BAFF3701
+// AA3DC074134DF11D31E2A421BAFF3701
+
         unsigned char hash[SHA256_DIGEST_LENGTH];
         uint32_t uniqueTime = time.getCurrentTimestamp();
-        std::string uniqueSeed = "00f24a5851e9372b87810a8e60cdd2e7cfd80b6e31";
-        //std::string uniqueTimeStr = std::to_string(uniqueTime);
-        //std::string uniqueSeed = uniqueTimeStr.append(util.getUniqueNum(10));
+        //std::string uniqueSeed = "00f24a5851e9372b87810a8e60cdd2e7cfd8026e31";
+        std::string uniqueTimeStr = std::to_string(uniqueTime);
+        std::string uniqueSeed = uniqueTimeStr.append(util.getUniqueNum(10));
         //7694 3b6ab625b4 bf97c99dcd 865cadd716 5b680ce66e 6482a0e8bf ee783198e0
         SHA256_CTX sha256;
         SHA256_Init(&sha256);
         SHA256_Update(&sha256, uniqueSeed.c_str(), uniqueSeed.size());
         SHA256_Final(hash, &sha256);
-//25162A752BF7DAAE72EAAF693675DAC589D6B0542FDA7A9F67EB5D7D9AF8D5D6
+
         this->mRsultPrivateKey.str(""); // Initialization
-        for(int i = 0; i < SHA256_DIGEST_LENGTH; i++)
-        {   
-            cout << "[DEBUG] " << endl;
+        for(int i=0; i<16; i++){
             this->mRsultPrivateKey << hex << setw(2) << setfill('0') << (int)hash[i];
         }
+
+        std::string PrivKeyRaw = this->mRsultPrivateKey.str();
+        std::transform(PrivKeyRaw.begin(), PrivKeyRaw.end(), PrivKeyRaw.begin(), ::toupper);
         
-        std::string test = this->mRsultPrivateKey.str();
-        std::transform(test.begin(), test.end(), test.begin(), ::toupper);
-        return test;
+        return PrivKeyRaw;
         
     }
 
